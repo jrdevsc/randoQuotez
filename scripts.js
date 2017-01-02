20e8e4c626ef5227ff36bb274cbad2887c9d6c14
julian rudolph <julian.rudolphsc@gmail.com>
2017-01-01 22:14:30 -0500

    added inital quote and get new quote button

--- scripts.js
+++ scripts.js
@@ -1,19 +1,73 @@
-$.ajax({
-  url: "http://api.forismatic.com/api/1.0/",
-    jsonp: "jsonp",
-    dataType: "jsonp",
-    data: {
-      method: "getQuote",
-      lang: "en",
-      format: "jsonp"
-    },
-    success: function(data){
-      if(data.quoteAutor == ""){
-          $('#myDiv h1').html("<h1>Author Unknown</h1>");
-      }else{
+// $.ajax({
+//   url: "http://api.forismatic.com/api/1.0/",
+//     jsonp: "jsonp",
+//     dataType: "jsonp",
+//     data: {
+//       method: "getQuote",
+//       lang: "en",
+//       format: "jsonp"
+//     },
+//     success: function(data){
+//       if(data.quoteAutor == ""){
+//           $('#myDiv h1').html("<h1>Author Unknown</h1>");
+//       }else{
+//         $('#myDiv h1').html("<h1>"+data.quoteAuthor+"</h1>");
+//         $('#myDiv h3').html("<h3>"+data.quoteText+"</h3>");
+//       }
+//       // console.log(data);
+//     }
+// });
+//pre populate first quote
+getFirstQuote();
+
+function getFirstQuote(){
+  $.ajax({
+    url: "http://api.forismatic.com/api/1.0/",
+      jsonp: "jsonp",
+      dataType: "jsonp",
+      data: {
+        method: "getQuote",
+        lang: "en",
+        format: "jsonp",
+      },
+      success: function(data){
         $('#myDiv h1').html("<h1>"+data.quoteAuthor+"</h1>");
         $('#myDiv h3').html("<h3>"+data.quoteText+"</h3>");
       }
-      // console.log(data);
+      });
     }
+
+
+$('#getQ').click(function(){
+  getQuote();
 });
+
+$('#tweetQ').click(function(){
+  tweetQ();
+});
+
+function getQuote(){
+  $.ajax({
+    url: "http://api.forismatic.com/api/1.0/",
+      jsonp: "jsonp",
+      dataType: "jsonp",
+      data: {
+        method: "getQuote",
+        lang: "en",
+        format: "jsonp"
+      },
+      success: function(data){
+        if(data.quoteAutor == ""){
+            $('#myDiv h1').html("<h1>Author Unknown</h1>");
+        }else{
+          $('#myDiv h1').html("<h1>"+data.quoteAuthor+"</h1>");
+          $('#myDiv h3').html("<h3>"+data.quoteText+"</h3>");
+        }
+        // console.log(data);
+      }
+  });
+}
+
+function tweetQ(){
+  console.log("tweetme function hit!");
+}
